4f9a704acad2a167ed13831a3eed74a1f8fb8517
marilynshen <89241212+marilynshen@users.noreply.github.com>
2022-02-24 00:37:33 -0600

    Add files via upload

---
+++ bigO.py
@@ -0,0 +1,58 @@
+#  Student Name: Marilyn Shen
+
+#  Student UT EID: mys467
+
+#  Partner Name: Jennifer Truong
+
+#  Partner UT EID: jat5244
+
+#  Course Name: CS 313E
+
+#  Unique Number: 51130
+
+#  Date Created: 2/22/2022
+
+#  Date Last Modified: 2/22/2022
+
+# Problem 1: Use python to create 3 different plots
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+msize = 5
+
+t = np.arange(0, msize, 0.1)
+
+# red dashes, blue squares and green triangles
+# plt.plot(t, t, 'r--', t, t**3.5 - 2**10, 'bs', t, 100*t**2.1 + 50, 'g^')
+
+plt.plot(t, (2 ** 10) * t + (2 ** 10), 'red', t, t ** 3.5 - 1000, 'blue', t,
+         100*t**2.1 + 50, 'green')
+
+plt.xlim(0, msize)
+plt.rcParams["figure.figsize"] = (7, 7)
+plt.show()
+
+
+msize = 15
+
+t = np.arange(0, msize, 0.1)
+
+plt.plot(t, (2 ** 10) * t + (2 ** 10), 'red', t, t ** 3.5 - 1000, 'blue', t,
+         100*t**2.1 + 50, 'green')
+
+plt.xlim(0, msize)
+plt.rcParams["figure.figsize"] = (7, 7)
+plt.show()
+
+
+msize = 50
+
+t = np.arange(0, msize, 0.1)
+
+plt.plot(t, (2 ** 10) * t + (2 ** 10), 'red', t, t ** 3.5 - 1000, 'blue', t,
+         100*t**2.1 + 50, 'green')
+
+plt.xlim(0, msize)
+plt.rcParams["figure.figsize"] = (7, 7)
+plt.show()
